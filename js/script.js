3f84aad306f6f95abfe6ae691ed95ebec18dd102
Evgeny-Chabrovsky <chabor111@gmail.com>
2021-04-24 22:54:55 +0300

    init project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+// let block = document.createElement("div");
+// block.classList.add("block");
+// document.querySelector(".slider").append(block);
